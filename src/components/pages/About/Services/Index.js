15ef29f112a6ae97c2e6d03f72db34460d7449fc
PedroHbs431 <pedrohenriquebgs1@gmail.com>
2022-05-10 19:26:57 -0300

    .

--- src/components/pages/About/Services/Index.js
+++ src/components/pages/About/Services/Index.js
@@ -9,12 +9,12 @@ const Services = () => {
             <h1 className="ServicesH1"> Ferramentas: </h1>
             <div className="ServicesWrapper">
                 <div className="ServicesCard">
-                    <a href='https://web.whatsapp.com/'><img className="ServicesIcon" alt="" src={Icon1}/>
+                    <a href='https://web.whatsapp.com/' rel="noreferrer" target="_blank"><img className="ServicesIcon" alt="" src={Icon1}/>
                     <h2 className="ServicesH2">Whatsapp</h2>
                     <p className="ServicesP">comunicação</p></a>
                 </div>
                 <div className="ServicesCard">
-                    <a href='https://parsec.app/downloads'><img className="ServicesIcon" alt="" src={Icon2}/>
+                    <a href='https://parsec.app/downloads' rel="noreferrer" target="_blank"><img className="ServicesIcon" alt="" src={Icon2}/>
                     <h2 className="ServicesH2">PARSEC</h2>
                     <p className="ServicesP">fazer download</p></a>
                 </div>

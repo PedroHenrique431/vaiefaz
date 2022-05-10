16ee1041f447bd022ef8e177f44522e1e49527bb
PedroHbs431 <pedrohenriquebgs1@gmail.com>
2022-05-10 14:01:31 -0300

    .

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroSection from '../../HeroSection';
-import { homeObjTwo, homeObjThree, homeObjFive } from '../HomePage/Data';
+import { homeObjFive } from '../HomePage/Data';
 
 function About() {
   return (

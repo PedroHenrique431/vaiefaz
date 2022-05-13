642a975a143213e7636dccc6afe70981594158f8
PedroHbs431 <pedrohenriquebgs1@gmail.com>
2022-05-13 11:03:25 -0300

    .

--- src/components/pages/About/faq/Data.js
+++ src/components/pages/About/faq/Data.js
@@ -2,14 +2,14 @@ export const Data = [
     
     {
       question: 'Como é feita a conexão?',
-      answer: 'Usamos um aplicativo chamado "Parsec". Ele é gratuito e suuuper leve.'
+      answer: 'Usamos um aplicativo chamado "Parsec". Ele é gratuito e suuuper leve. Com ele, o técnico poderá acessar sua máquina remotamente para fazer os ajustes necessários (assim que você conceder permissão a ele).'
     },
     {
-      question: 'Como faço para adiquirir o Parsec?',
-      answer: 'Basta fazer o download através do link nesta página. O aplicativo está disponível para Windows, Linux e macOS.'
+      question: 'Posso desinstalar o Parsec após os procedimentos?',
+      answer: 'Sim! O programa só será usado uma vez. Após isso, você é livre pra usá-lo como quiser, ou removê-lo.'
     },
     {
-      question: 'Depois de quanto tempo entrarão em contato comigo?',
+      question: 'Depois de quanto tempo meu chamado será respondido?',
       answer: 'Até 1 hora partindo do momento em que sua requisição for enviada.'
     }
   ];
